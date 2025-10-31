e0e2a90354c5dd36e9a9406204153365a1fcfbea
Vega <bryanvegaaguilar@gmail.com>
2025-10-31 09:28:54 -0600

    Primer Commit

---
+++ src/main/java/com/example/Hirelance/HirelanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hirelance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirelanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirelanceApplication.class, args);
+	}
+
+}
